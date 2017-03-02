41c14cf950cb02536ce6733cfc285fe67340e695
齋藤卓馬 <saitoutakuma@saitoutakumanoMacBook-Pro.local>
2017-03-02 17:32:30 +0900

    add OtherView

---
+++ Anetos/Anetos/AboutViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AboutViewController.swift
+//  Anetos
+//
+//  Created by 齋藤卓馬 on 2017/03/02.
+//  Copyright © 2017年 齋藤卓馬. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ Anetos/Anetos/GuideViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GuideViewController.swift
+//  Anetos
+//
+//  Created by 齋藤卓馬 on 2017/03/02.
+//  Copyright © 2017年 齋藤卓馬. All rights reserved.
+//
+
+import UIKit
+
+class GuideViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
