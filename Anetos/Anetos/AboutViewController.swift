baa377b22c6387b5e2e468ee6423940ad2a477b4
齋藤卓馬 <saitoutakuma@saitoutakumanoMacBook-Pro.local>
2017-03-11 16:30:56 +0900

    add About Anetos

--- Anetos/Anetos/AboutViewController.swift
+++ Anetos/Anetos/AboutViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class AboutViewController: UIViewController {
+class AboutViewController: UIViewController, UIWebViewDelegate {
+    
+   
+    
+    @IBOutlet weak var termsWebView: UIWebView!
+    
+    var targetURL = NSURL(string:"http://52.196.123.118:3000/terms")
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.termsWebView.delegate = self
+        
+        let request = NSURLRequest(url: targetURL as! URL)
+        self.termsWebView.loadRequest(request as URLRequest)
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
