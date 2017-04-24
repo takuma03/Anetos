7a25d7cf7665eac2e08f383c5f9363d47dd8492b
齋藤卓馬 <saitoutakuma@saitoutakumanoMacBook-Pro.local>
2017-04-24 21:43:37 +0900

    登録ボタンを押すとクラッシュする問題解消

---
+++ Anetos/Anetos/RegisterClothViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegisterClothViewController.swift
+//  Anetos
+//
+//  Created by 齋藤卓馬 on 2017/04/24.
+//  Copyright © 2017年 齋藤卓馬. All rights reserved.
+//
+
+import UIKit
+
+class RegisterClothViewController: UIViewController {
+    
+    
+    @IBAction func unwindToRegisterCloth(segue: UIStoryboardSegue){
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
