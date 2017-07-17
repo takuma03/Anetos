30425d37c07333874499e0f9daab52150b43cee8
齋藤卓馬 <saitoutakuma@saitoutakumanoMacBook-Pro.local>
2017-07-17 18:44:32 +0900

    ホーム画面推奨洋服表示機能作成

--- Anetos/Anetos/FirstViewController.swift
+++ Anetos/Anetos/FirstViewController.swift
@@ -10,19 +10,14 @@ import UIKit
 
 class FirstViewController: UIViewController, UICollectionViewDataSource{
     
-    var clothes:[Cloth] = [Cloth]()
-    
+    var tops_clothes:[Cloth] = [Cloth]()
+    var others_clothes:[Cloth] = [Cloth]()
     @IBOutlet weak var TopsCollectionView: UICollectionView!
     @IBOutlet weak var OthersCollectionView: UICollectionView!
-
     @IBOutlet weak var bottomsimage: UIImageView!
-    
     @IBOutlet weak var home_region: UILabel!
-    
     @IBOutlet weak var temp: UILabel!
-    
     @IBOutlet weak var tenki: UILabel!
-    
     @IBOutlet weak var weather_img: UIImageView!
     
     
@@ -87,16 +82,24 @@ class FirstViewController: UIViewController, UICollectionViewDataSource{
     }
     func setupClothes() {
         //配列で取得したcloth_id分処理を繰り返す
-        for cloth_id in self.appDelegate.cloth_array{
+        for cloth_id in self.appDelegate.tops_cloth_array{
             let f = Cloth(imageUrl: NSURL(string: "http://52.193.213.154:3000/clothes/get_image?id=" + cloth_id))
-            clothes.append(f)
+            tops_clothes.append(f)
+        }
+        
+        for cloth_id in self.appDelegate.others_cloth_array{
+            let f = Cloth(imageUrl: NSURL(string: "http://52.193.213.154:3000/clothes/get_image?id=" + cloth_id))
+            others_clothes.append(f)
         }
     }
     
     //データの個数を返すメソッド
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
-    {
-        return clothes.count
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == TopsCollectionView {
+            return tops_clothes.count
+        }else{
+            return others_clothes.count
+        }
     }
     
 
@@ -104,38 +107,23 @@ class FirstViewController: UIViewController, UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if collectionView == TopsCollectionView {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TopsCell", for: indexPath as IndexPath) as! HomeCollectionViewCell
-//            セルの背景色をランダムに設定する。
-                        cell.backgroundColor = UIColor(red: CGFloat(drand48()),
-                                                       green: CGFloat(drand48()),
-                                                       blue: CGFloat(drand48()),
-                                                       alpha: 1.0)
-                        cell.setCell(cloth: clothes[indexPath.row])
-            
+                        cell.setCell(cloth: tops_clothes[indexPath.row])
                         return cell
         }
         else {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OthersCell", for: indexPath as IndexPath) as! HomeCollectionViewCell
-            
-            
-                        cell.backgroundColor = UIColor(red: CGFloat(drand48()),
-                                                        green: CGFloat(drand48()),
-                                                        blue: CGFloat(drand48()),
-                                                        alpha: 1.0)
-            
-            
-                        cell.setCell2(cloth: clothes[indexPath.row])
+                        cell.setCell2(cloth: others_clothes[indexPath.row])
                         return cell
         }
     }
     
      //ボトムスを表示させるメソッド
     func setupBottoms(){
-        let imgUrl = NSURL(string: "http://52.193.213.154:3000/clothes/get_image?id=40");
+        let imgUrl = NSURL(string: "http://52.193.213.154:3000/clothes/get_image?id=" + self.appDelegate.bottoms_cloth);
         // ファイルデータを作る
         let file = NSData(contentsOf: imgUrl! as URL);
         // イメージデータを作る
         let img = UIImage(data:file! as Data)
-        
         // イメージビューに表示する
         bottomsimage.image = img
     }
@@ -144,8 +132,7 @@ class FirstViewController: UIViewController, UICollectionViewDataSource{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-//        TopsCollectionView.delegate = self
-//        OthersCollectionView.delegate = self
+
         
         TopsCollectionView.dataSource = self
         OthersCollectionView.dataSource = self
@@ -166,6 +153,4 @@ class FirstViewController: UIViewController, UICollectionViewDataSource{
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
